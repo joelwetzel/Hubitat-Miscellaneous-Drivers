2e74b95007ea02329c59f4331f3d6cd3fd982dea
Joel Wetzel <jwetzel@affirma.com>
2023-11-30 11:52:35 -0600

    Cleaning up after linting

--- httpSwitch.groovy
+++ httpSwitch.groovy
@@ -5,83 +5,110 @@
  *
  */
 metadata {
-    definition(name: "Http Switch", namespace: "joelwetzel", author: "Joel Wetzel") {
-        capability "Actuator"
-        capability "Switch"
-        capability "Sensor"
-        capability "Refresh"
+    definition(name: 'Http Switch', namespace: 'joelwetzel', author: 'Joel Wetzel') {
+        capability 'Actuator'
+        capability 'Switch'
+        capability 'Sensor'
+        capability 'Refresh'
     }
 }
 
 preferences {
-    section("URIs") {
-        input "onURI", "text", title: "On URI", required: false
-        input "offURI", "text", title: "Off URI", required: false
-        input "refreshURI", "text", title: "Refresh URI", required: false
-        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+    section('URIs') {
+        input 'onURI', 'text', title: 'On URI', required: false
+        input 'offURI', 'text', title: 'Off URI', required: false
+        input 'refreshURI', 'text', title: 'Refresh URI', required: false
+        input name: 'logEnable', type: 'bool', title: 'Enable debug logging', defaultValue: true
     }
 }
 
+public enum ConstNames {
+
+    LOGENABLE('logEnable'),
+    SWITCH('switch')
+
+}
+
+public enum ConstValues {
+
+    TRUE('true'),
+    FALSE('false'),
+    ON('on'),
+    OFF('off')
+
+}
+
+public enum ConstTypes {
+
+    BOOL('bool'),
+    STRING('string')
+
+}
+
 def logsOff() {
-    log.warn "debug logging disabled..."
-    device.updateSetting("logEnable", [value: "false", type: "bool"])
+    log.warn 'debug logging disabled...'
+    device.updateSetting(ConstNames.LOGENABLE, [value: ConstValues.FALSE, type: ConstTypes.BOOL])
 }
 
 def updated() {
-    log.info "updated..."
+    log.info 'updated...'
     log.warn "debug logging is: ${logEnable == true}"
-    if (logEnable) runIn(1800, logsOff)
+    if (logEnable) {
+        runIn(1800, logsOff)
+    }
 }
 
 def parse(String description) {
-    if (logEnable) log.debug(description)
+    if (logEnable) {
+        log.debug(description)
+    }
 }
 
 def on() {
     if (logEnable) {
-      log.debug "Sending on GET request to [${settings.onURI}]"
+        log.debug "Sending on GET request to [${settings.onURI}]"
     }
 
     try {
         httpGet(settings.onURI) { resp ->
             if (resp.success) {
-                sendEvent(name: "switch", value: "on", isStateChange: true)
+                sendEvent(name: ConstNames.SWITCH, value: ConstValues.ON, isStateChange: true)
             }
             if (logEnable) {
                 if (resp.data) {
-                  log.debug "${resp.data}"
+                    log.debug "${resp.data}"
                 }
             }
         }
-    } catch (Exception e) {
+    } catch (e) {
         log.warn "Call to on failed: ${e.message}"
     }
 }
 
 def off() {
     if (logEnable) {
-      log.debug "Sending off GET request to [${settings.offURI}]"
+        log.debug "Sending off GET request to [${settings.offURI}]"
     }
 
     try {
         httpGet(settings.offURI) { resp ->
             if (resp.success) {
-                sendEvent(name: "switch", value: "off", isStateChange: true)
+                sendEvent(name: ConstNames.SWITCH, value: ConstValues.OFF, isStateChange: true)
             }
             if (logEnable) {
                 if (resp.data) {
-                  log.debug "${resp.data}"
+                    log.debug "${resp.data}"
                 }
             }
         }
-    } catch (Exception e) {
+    } catch (e) {
         log.warn "Call to off failed: ${e.message}"
     }
 }
 
 def refresh() {
     if (logEnable) {
-      log.debug "Sending refresh GET request to [${settings.refreshURI}]"
+        log.debug "Sending refresh GET request to [${settings.refreshURI}]"
     }
 
     try {
@@ -89,20 +116,19 @@ def refresh() {
             if (resp && resp.status == 200) {
               // Parse the response and send the correct event
 
-              def result = resp.data
-              if (result == "true") {
-                  sendEvent(name: "switch", value: "on", isStateChange: true)
+                if (resp.data == ConstValues.TRUE) {
+                    sendEvent(name: ConstNames.SWITCH, value: ConstValues.ON, isStateChange: true)
               } else {
-                  sendEvent(name: "switch", value: "off", isStateChange: true)
-              }
+                    sendEvent(name: ConstNames.SWITCH, value: ConstValues.OFF, isStateChange: true)
+                }
             }
             if (logEnable) {
                 if (resp.data) {
-                  log.debug "${resp.data}"
+                    log.debug "${resp.data}"
                 }
             }
         }
-    } catch (Exception e) {
+    } catch (e) {
         log.warn "Call to refresh failed: ${e.message}"
     }
 }
